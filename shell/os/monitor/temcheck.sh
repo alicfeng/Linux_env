4789e7f26fe797aa49b07da35ec8507c61311849
alic <fengalic@gmail.com>
2017-05-13 11:59:42 +0800

    Unix/Linux温度数据读取并实现桌面通知

--- shell/os/monitor/temcheck.sh
+++ shell/os/monitor/temcheck.sh
@@ -3,6 +3,9 @@
 # PS 推荐结合定时任务crontab每分钟执行一次
 # * * * * * bash /home/alic/tutorial/Github/Linux_env/shell/os/monitor/temcheck.sh run
 
+# 配置警告时的温度
+warningTem=80
+
 # 安装 func
 function i(){
     sudo apt-get install lm-sensors -y && sudo modprobe coretemp
@@ -12,14 +15,16 @@ function i(){
 function todo(){
     str=`sensors |awk '{print $2}'| sed -n '3p'`
     tem=${str:1:2}
-    if [ $tem -gt 50 ]
+    if [ $tem -gt $warningTem ]
     then
-    notify-send -i dialog-warning "系统温度提醒" "$(whoami) 你的电脑温度过高 \n Quickly To Kill Your Bad Process"
+    # 解决crontab中的notify-send 不能显示通知的处理方法 next line
+    export DISPLAY=:0.0 && \
+    notify-send -i dialog-warning "系统温度提醒" "$(whoami) 你的电脑温度过高 ↦ $warningTem°C \n Quickly To Kill Your Bad Process"
     fi 
 }
 # 为了定时方便，程序执行一分钟 func
 function run(){
-    for((i=1;i<=12;i++));do
+    for((i=1;i<=10;i++));do
         todo
         sleep 5
     done
