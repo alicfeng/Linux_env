9cfd3dc4bec22403932f0796a3c6d8a4be080f3a
alic <fengalic@gmail.com>
2017-05-16 20:16:52 +0800

    gui install helper

--- shell/os/install/helper.sh
+++ shell/os/install/helper.sh
@@ -2,6 +2,7 @@
 # AlicFeng https://www.samego.com alic@samego.com 价值源于技术，技术源于分享
 # build date 2017.05.16
 # last modified date 2017.05.16
-wget https://github.com/alicfeng/Linux_env/blob/master/shell/os/install/install.sh && \
-wget https://github.com/alicfeng/Linux_env/blob/master/shell/os/install/open_alicfeng_github.py && \
+
+wget https://raw.githubusercontent.com/alicfeng/Linux_env/master/shell/os/install/install.sh && \
+wget https://raw.githubusercontent.com/alicfeng/Linux_env/master/shell/os/install/open_alicfeng_github.py && \
 bash install.sh
