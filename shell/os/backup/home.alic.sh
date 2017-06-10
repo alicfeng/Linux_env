e2f7adea77a36e943bcb2f3797a1fcdac1b2031d
alicfeng <alic@samego.com>
2017-06-10 14:36:52 +0800

    apache2-tp3

---
+++ shell/os/backup/home.alic.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+# 备份个人目录的所有文件(不包含隐藏文件)
+backup=/media/alic/share/backup/$(date +%Y-%m-%d)
+mkdir -p $backup
+
+# 想看过程就加个v，想跳过某些文件就用--exclude，比如：
+tar --exclude '.*' -cvf - /home/alic | ( cd $backup; tar -xf -)
+
+export DISPLAY=:0.0 && \
+notify-send -t 0 -i utilities-terminal "AShellTools快速配置工具" "$USER☺备份完成"
+
