1c3464b97256f242c9fee23e70eaaaa7f785e4fc
alicfeng <alic@samego.com>
2017-06-27 21:04:04 +0800

    apache conf

--- server/build/lanmp/build-14.04-32os.sh
+++ server/build/lanmp/build-14.04-32os.sh
@@ -25,4 +25,4 @@ sudo apt-get install php5-gd curl libcurl3 libcurl3-dev php5-curl php5-dev libpc
 php5 libapache2-mod-php5 php5-mysql  php5-gd php5-intl php-pear php5-imagick && \
 php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode && \
 php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl php5-cgi libapache2-mod-auth-mysql && \
-libpcre3 
+libpcre3 libpcre3-dev
